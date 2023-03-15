5a125b2203e182aa95d811343708b7c54ac8e947
Fesci <tomasfesci@gmail.com>
2023-03-15 20:38:02 -0300

    bug fixed

--- src/routes/pokeRouter.js
+++ src/routes/pokeRouter.js
@@ -4,22 +4,20 @@ const { Pokemon, Type } = require("../db");
 const { checkPostData, getAllPokemons, getPokeByName } = require("../controller/getPokemons");
 pokeRouter.get("/", async (req, res) => {
   const { name } = req.query;
-  if (name) {
-    const poke = await getPokeByName(name);
-    res.status(200).send(poke);
-  } else {
-    try {
-      let allPokemons = await getAllPokemons();
-
-      res.status(200).send(allPokemons);
-    } catch (error) {
-      res.status(404).send(error.message);
+  try {
+    if (name) {
+      const poke = await getPokeByName(name);
+      res.status(200).send(poke);
     }
+    let allPokemons = await getAllPokemons();
+
+    res.status(200).send(allPokemons);
+  } catch (error) {
+    res.status(404).send({ error: "No pokemon found" });
   }
 });
 pokeRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
-
   let allPokemons = await getAllPokemons();
   if (id) {
     const poke = allPokemons.find(p => id == p.id);
@@ -42,7 +40,9 @@ pokeRouter.post("/", checkPostData, async (req, res) => {
 
 pokeRouter.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  const poke = Pokemon.findByPk(id);
+  const poke = Pokemon.findOne({
+    where: { id: id },
+  });
   if (poke) {
     await Pokemon.destroy({
       where: { id: id },
@@ -52,6 +52,11 @@ pokeRouter.delete("/:id", async (req, res) => {
     res.status(404).send({ error: "Pokemon not found" });
   }
 });
+
 //TODO
-pokeRouter.put("/:id", async (req, res) => {});
+pokeRouter.put("/:id", async (req, res) => {
+  const data = req.body;
+  if (data) {
+  }
+});
 module.exports = pokeRouter;
